e3b6226c676427e25e40a750874e5345d89e6909
AcunaF <facuna30@gmail.com>
2023-12-11 12:25:58 -0300

    login y validaciones

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+const { response } = require("express");
+
+const login = async (req, res = response) => {
+  res.json({
+    msg: "login ok",
+  });
+};
+
+module.exports = {
+  login,
+};
---
+++ routes/auth.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const { login } = require("../controllers/auth.controller");
+const { check } = require("express-validator");
+const { validateFields } = require("../middlewares/validateFields");
+
+const router = Router();
+
+router.post(
+  "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").not().isEmpty(),
+    validateFields,
+  ],
+
+  login
+);
+
+module.exports = router;
